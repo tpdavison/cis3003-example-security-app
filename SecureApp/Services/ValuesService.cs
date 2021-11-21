f7a672efb4030f1d9e2a8afa698b938b66321a2a
Tyrone Davison <t.p.davison@tees.ac.uk>
2021-11-21 14:27:33 +0000

    Add bearer auth for api calls

--- SecureApp/Services/ValuesService.cs
+++ SecureApp/Services/ValuesService.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Auth0.AuthenticationApi;
+using Auth0.AuthenticationApi.Models;
 using Microsoft.Extensions.Configuration;
 
 namespace SecureApp.Services
@@ -20,8 +23,23 @@ namespace SecureApp.Services
         public async Task<ValuesGetDto> Get()
         {
             var client = ClientFactory.CreateClient();
+
+            var auth0AuthenticationClient = new AuthenticationApiClient(
+                new Uri($"https://{Configuration["Auth:Domain"]}/"));
+            var tokenRequest = new ClientCredentialsTokenRequest()
+            {
+                ClientId = Configuration["Auth:ClientId"],
+                ClientSecret = Configuration["Auth:ClientSecret"],
+                Audience = Configuration["Services:Values:AuthAudience"]
+            };
+            var tokenResponse =
+                await auth0AuthenticationClient.GetTokenAsync(tokenRequest);
+
             var baseAddress = Configuration["Services:Values:BaseAddress"];
             client.BaseAddress = new Uri(baseAddress);
+
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", tokenResponse.AccessToken);
 
             var response = await client.GetAsync("api/values");
             response.EnsureSuccessStatusCode();
